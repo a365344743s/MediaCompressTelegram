130a245e6af55f6c6d0eb2d6be064542d35c2b3b
wangshuai01 <wangshuai01@comisys.net>
2022-07-19 10:16:01 +0800

    remove unused include

--- telegram/jni/gifvideo.cpp
+++ telegram/jni/gifvideo.cpp
@@ -4,13 +4,10 @@
 #include "c_utils.h"
 
 extern "C" {
-#include <libavformat/avformat.h>
 #include <libavformat/isom.h>
 #include <libavcodec/bytestream.h>
 #include <libavcodec/get_bits.h>
-#include <libavcodec/golomb.h>
 #include <libavutil/eval.h>
-#include <libavutil/intmath.h>
 #include <libswscale/swscale.h>
 }
 
